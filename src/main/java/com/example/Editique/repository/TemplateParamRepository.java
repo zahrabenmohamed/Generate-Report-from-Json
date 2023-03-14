4567c6db7b0e6aeb7149e7b39f42a8eaa65e5d6c
zahrabenmohamed <benmedzahra20@gmail.com>
2023-03-14 16:38:41 +0100

    create Repository

---
+++ src/main/java/com/example/Editique/repository/TemplateParamRepository.java
@@ -0,0 +1,7 @@
+package com.example.Editique.repository;
+
+import com.example.Editique.entity.TemplateParam;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TemplateParamRepository extends JpaRepository<TemplateParam,Long> {
+}
