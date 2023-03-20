cca47cbaf751a835db10c70b0f70a209b74e233f
zahrabenmohamed <benmedzahra20@gmail.com>
2023-03-20 12:16:47 +0000

    comment the code

--- src/main/resources/database/DML.sql
+++ src/main/resources/database/DML.sql
@@ -7,10 +7,17 @@ VALUES (1,'facture', 'this is an invoice', 'facture.jrxml'),
 --insertion in table of  template-param
 INSERT INTO template_param (id, name, description, type, selector_type, selector, source)
 VALUES
-    (1, 'client_fname', 'This is a sample parameter', 'java.lang.String', 'JSON' ,'input/param1', 'customer'),
-    (2, 'client_lname', 'This is a sample parameter', 'java.lang.String', 'JSON', 'input/param2', 'customer'),
+    (1, 'client_fname', 'This is a sample parameter', 'java.lang.String', 'JSON' ,'${name}', 'customer'),
+    (2, 'client_lname', 'This is a sample parameter', 'java.lang.String', 'JSON', '${name}', 'customer'),
     (3, 'account', 'This is a sample parameter', 'java.lang.String', 'JSON', 'input/param3', 'customer'),
     (4, 'agency', 'This is a sample parameter', 'java.lang.String', 'JSON','input/param4', 'customer'),
-    (5, 'transactions', 'This is a sample parameter', 'ArrayList', 'JSON','input/param5', 'transactions'),
-    (6, 'openingDate', 'This is a sample parameter', 'java.lang.String', 'JSON','input/param6', 'customer');
+    (5, 'transactions', 'This is a sample parameter', 'ArrayList', 'JSON','${data.id}', 'transactions'),
+    (6, 'openingDate', 'This is a sample parameter', 'java.lang.String', 'JSON','input/param6', 'customer'),
+
+INSERT INTO template_template_param(template_id,template_param_id) VALUES(3,2),
+                                                                         (3,3),
+                                                                         (3,4),
+                                                                         (3,5),
+                                                                         (3,6);
+
 
